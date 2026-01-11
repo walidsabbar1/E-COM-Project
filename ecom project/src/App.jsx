2e24f3b4c76a66f6d0fd80aa87ec91f4abe70847
yahya <m130109177@taalim.ma>
2026-01-11 20:09:56 +0100

    top selling new arivall

--- ecom project/src/App.jsx
+++ ecom project/src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import NewArrivals from "./components/NewArrivals";
+import TopSelling from "./components/TopSelling";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <NewArrivals />
+      <TopSelling />
     </>
   );
 }
